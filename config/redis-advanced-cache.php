686f551ac5a84cf1a2aa2bb06496f113ad0b6581
c.jordan <c.jordan@xefi.fr>
2025-10-08 17:58:30 +0200

    Up

--- config/redis-advanced-cache.php
+++ config/redis-advanced-cache.php
@@ -28,7 +28,7 @@ return [
     'connection' => [
         'host' => env('REDIS_HOST', '127.0.0.1'),
         'port' => env('REDIS_PORT', 6379),
-        'password' => env('REDIS_PASSWORD', 'null'),
+        'password' => env('REDIS_PASSWORD', null),
         'database' => env('REDIS_DB', 1),
         'scheme' => env('REDIS_ADVANCED_CACHE_SCHEME', 'tcp'),
     ],
