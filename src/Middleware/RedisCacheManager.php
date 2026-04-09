9ac14176d208f202c3211a803891725564af5cb3
root <root@RouxCool.localdomain>
2026-04-10 01:02:44 +0200

    Release v1.2.15

--- src/Middleware/RedisCacheManager.php
+++ src/Middleware/RedisCacheManager.php
@@ -31,7 +31,6 @@ class RedisCacheManager
      */
     public function __construct()
     {
-        \Log::info("manager");
         $this->cacheService = RedisCacheService::getInstance();
         $this->redis = $this->cacheService->getRedis();
 
